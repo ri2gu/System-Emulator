3a26611cb9805f362e272b65f94721da9be909a5
Ritu Gupta <rigupta@cs.utexas.edu>
2023-04-11 17:27:27 -0500

    fixed halt in hazard

--- src/pipe/hazard_control.c
+++ src/pipe/hazard_control.c
@@ -83,8 +83,8 @@ void pipe_control_stage(proc_stage_t stage, bool bubble, bool stall) {
 comb_logic_t handle_hazards(opcode_t D_opcode, uint8_t D_src1, uint8_t D_src2, 
                             opcode_t X_opcode, uint8_t X_dst, bool X_condval) {
     /* Students: Change this code */
-    bool f_stall = F_out->status == STAT_HLT || F_out->status == STAT_INS; 
-    pipe_control_stage(S_FETCH, false, false);
+    bool f_stall = F_out->status == STAT_HLT || F_out->status == STAT_INS;
+    pipe_control_stage(S_FETCH, false, f_stall);
     pipe_control_stage(S_DECODE, false, false);
     pipe_control_stage(S_EXECUTE, false, false);
     pipe_control_stage(S_MEMORY, false, false);
